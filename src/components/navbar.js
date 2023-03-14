eb34fb02cc297ee81f58f45c13b58d3651ddbd61
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-14 23:27:36 +0600

    First

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,16 @@ import {AiOutlineMenu} from 'react-icons/ai';
 const Navbar = () => {
     return (
         <div>
+            {/* Left side */}
             <div>
                 <div>
-                    <AiOutlineMenu />
+                    <AiOutlineMenu size={30}/>
+                </div>
+                <h1>
+                    Best <span>Eats</span>
+                </h1>
+                <div>
+                    
                 </div>
             </div>
         </div>
