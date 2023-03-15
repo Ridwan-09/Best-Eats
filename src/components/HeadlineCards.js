5b53ea73a6636421e5d14825406a0513191bdd26
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-15 23:18:49 +0600

    First

--- src/components/HeadlineCards.js
+++ src/components/HeadlineCards.js
@@ -3,7 +3,16 @@ import React from 'react';
 const HeadlineCards = () => {
     return (
         <div>
-            
+            {/* Card */}
+            <div>
+                {/* Overlay */}
+                <div>
+                    <p>Sun's Out, BOGO's Out</p>
+                    <p>Through 8/26</p>
+                    <button>Order Now</button>
+                </div>
+                <img src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+            </div>
         </div>
     );
 };
