772e0d0ae237781d4f80169ac283551b7871cfbc
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-15 23:31:31 +0600

    First

--- src/components/HeadlineCards.js
+++ src/components/HeadlineCards.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const HeadlineCards = () => {
     return (
-        <div>
+        <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
             {/* Card */}
-            <div>
+            <div className='rounded-xl relative'>
                 {/* Overlay */}
-                <div>
-                    <p>Sun's Out, BOGO's Out</p>
-                    <p>Through 8/26</p>
-                    <button>Order Now</button>
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
                 </div>
                 <img src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
             </div>
