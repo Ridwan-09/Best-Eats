bedea65d42eba13ce818c91ca118e058fa9ea4ef
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-16 00:12:20 +0600

    First

--- src/components/HeadlineCards.js
+++ src/components/HeadlineCards.js
@@ -17,21 +17,21 @@ const HeadlineCards = () => {
             <div className='rounded-xl relative'>
                 {/* Overlay */}
                 <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
-                    <p className='px-2'>Through 8/26</p>
+                    <p className='font-bold text-2xl px-2 pt-4'>New Restaurant</p>
+                    <p className='px-2'>Added Daily</p>
                     <button className='border-white bg-white text-black mx-2 p-2 absolute bottom-4'>Order Now</button>
                 </div>
-                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/6294465/pexels-photo-6294465.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
             </div>
             {/* Card */}
             <div className='rounded-xl relative'>
                 {/* Overlay */}
                 <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
-                    <p className='px-2'>Through 8/26</p>
+                    <p className='font-bold text-2xl px-2 pt-4'>We Deliver Deserts Too</p>
+                    <p className='px-2'>Tasty Treats</p>
                     <button className='border-white bg-white text-black mx-2 p-2 absolute bottom-4'>Order Now</button>
                 </div>
-                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/5710173/pexels-photo-5710173.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
             </div>
         </div>
     );
