ff129c0d8f3c9b08999b4eb65f0bfcdf4f65fe31
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-15 10:14:02 +0600

    First

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai';
+import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose} from 'react-icons/ai';
+import {BsFillCartFill} from 'react-icons/bs';
+import {TbTruckDelivery} from 'react-icons/tb';
+
 const Navbar = () => {
     return (
         <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
@@ -22,6 +25,39 @@ const Navbar = () => {
             <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
                 <AiOutlineSearch size={25}/>
                 <input className='bg-transparent p-2 w-full focus:outline-none' type="text" placeholder='Search food' />
+            </div>
+            {/* Cart button */}
+
+            <button className='bg-black text-white hidden md:flex items-center p-5 rounded-full'>
+                <BsFillCartFill size={20} className='mr-w'/> Cart
+            </button>
+
+            {/* Mobile Menu */}
+            {/* Overlay */}
+            <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+
+            {/* Side drawer menu */}
+            <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
+            <AiOutlineClose 
+            size={30} 
+            className='absolute right-4 top-4 cursor-pointer' 
+            />
+            <h2 className='text-2xl p-4'>
+                Best <span className='font-bold'>Eats</span>
+            </h2>
+
+            <nav>
+                <ul className='flex flex-col p-4 text-gray-800'>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Orders</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Favourites</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Wallet</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Help</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Promotions</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Best Ones</li>
+                    <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Invite Friends</li>
+                </ul>
+            </nav>
+
             </div>
         </div>
     );
