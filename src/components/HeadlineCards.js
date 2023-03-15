1eb2f61a0796e011147adfb1cbbc26742ed19c65
Ridwan Salim <ridwan.salim@gmail.com>
2023-03-15 23:48:05 +0600

    First

--- src/components/HeadlineCards.js
+++ src/components/HeadlineCards.js
@@ -9,7 +9,27 @@ const HeadlineCards = () => {
                 <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
                     <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
                     <p className='px-2'>Through 8/26</p>
-                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                    <button className='border-white bg-white text-black mx-2 p-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+            </div>
+            {/* Card */}
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 p-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+            </div>
+            {/* Card */}
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 p-2 absolute bottom-4'>Order Now</button>
                 </div>
                 <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.pexels.com/photos/824635/pexels-photo-824635.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
             </div>
